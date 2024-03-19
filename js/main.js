fb9f557a39fc0111f551aac20de34aa4cef4e8f1
buwaneka-oshajith <buwaneka.oshajith@gmail.com>
2024-03-19 17:02:17 +0530

    sponser chnages

--- js/main.js
+++ js/main.js
@@ -224,3 +224,35 @@ function countUp(targetElement, targetValue) {
   countUp(resortCountElement, 120);
   countUp(brandCountElement, 10000);
 
+ /**
+   * Gallery Slider
+   */
+ new Swiper('.gallery-slider', {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      }
+    }
+  });
